6c8e00e2a21efe14a4a60d0ab8fa2d07c569e2c9
Anthony Sottile <asottile@yelp.com>
2016-07-19 22:19:05 -0700

    Work creds

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-set -ex
-SAN_MATEO_CALTRAIN='37.5678222,-122.3259203'
 exec ./venv/bin/python example.py \
-    --username asottilehome \
-    --location $SAN_MATEO_CALTRAIN \
-    --step-limit 5 \
+    --username asottileyelp \
+    --location '140 new montgomery, san francisco, ca' \
+    --step-limit 3 \
     --auto_refresh 10 \
-    --ignore zubat,rattata,pidgey,doduo,weedle,caterpie \
-    --zoom 16 \
+    --ignore zubat \
+    --zoom 15 \
     --host 0.0.0.0 \
-    --port 5000
+    --port 5000 \
